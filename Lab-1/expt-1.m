4e2b5b1d744063267e126d676af3d5f03feab02b
Aditya Sinha <adityaishan007@gmail.com>
2025-07-10 12:10:13 +0530

    Experiment 1

---
+++ Lab-1/expt-1.m
@@ -0,0 +1,27 @@
+%Experiment 1 
+%PRN : 24070123006
+%A1
+
+A = [9 , 1 ; 5 , 7]
+B = [1 , 3 ; 0 , 5]
+
+% Matrix Addition
+Add = A + B
+
+% Matrix Subtraction
+Sub = A - B
+
+% Matrix Multiplication
+Mul = A * B
+
+syms t a b c;
+
+o = t^3
+j = exp(a*t)
+d = (5*exp(2*t)-3)^2
+e = 3*(t^4)-2*(t^3) + 4*exp(-3*t) - 2*sin(5*t) +3* cos(2*t)
+f = 5*cos(5*t)
+g = cosh(b*t)
+h = sin(7*t)
+i = sinh (c*t)
+
